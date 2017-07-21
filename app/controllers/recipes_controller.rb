2f7c9579a9f7d6d13d78ed20e3c4ec8379a012bd
Felipe <feload@gmail.com>
2017-07-21 21:51:03 +0000

    Add list of recipes

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,7 +1,19 @@
 class RecipesController < ApplicationController
   
+  before_action :set_recipe, only: [:show]
+  
   def index
     @recipes = Recipe.all
   end 
   
-end 
+  def show
+    @recipe = set_recipe
+  end
+  
+  private
+  
+  def set_recipe
+    Recipe.find(params[:id])
+  end
+  
+end
--- test/integration/recipes_test.rb
+++ test/integration/recipes_test.rb
@@ -17,6 +17,16 @@ class RecipesTest < ActionDispatch::IntegrationTest
     get recipes_path
     assert_template 'recipes/index'
     assert_match @recipe.name, response.body
+    assert_select "a[href=?]", recipe_path(@recipe), @recipe.name
     assert_match @recipe2.name, response.body
+    assert_select "a[href=?]", recipe_path(@recipe2), @recipe2.name
+  end
+  
+  test "should get recipes show" do
+    get recipe_path @recipe
+    assert_template 'recipes/show'
+    assert_match @recipe.name, response.body
+    assert_match @recipe.description, response.body
+    assert_match @chef.name, response.body
   end
 end
