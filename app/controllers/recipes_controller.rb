9ee8cfc053c0d393dcea05132c758b11e9c9c7f3
Felipe <feload@gmail.com>
2017-07-22 20:00:13 +0000

    Add new recipe form

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -10,10 +10,29 @@ class RecipesController < ApplicationController
     @recipe = set_recipe
   end
   
+  def new
+    @recipe = Recipe.new
+  end
+  
+  def create
+    @recipe = Recipe.new(get_params)
+    @recipe.chef = Chef.first
+    if @recipe.save
+      flash[:notice] = "Recipe created correctly"
+      redirect_to recipe_path(@recipe)
+    else
+      render 'new' 
+    end
+  end
+  
   private
   
   def set_recipe
     Recipe.find(params[:id])
+  end
+  
+  def get_params
+    params.require(:recipe).permit(:name, :description)
   end
   
 end
--- test/models/recipe_test.rb
+++ test/models/recipe_test.rb
@@ -20,6 +20,7 @@ class RecipeTest < ActionDispatch::IntegrationTest
     @recipe.name = " "
     assert_not @recipe.valid?
   end
+  
   test "Description should be present" do
     @recipe.description = " "
     assert_not @recipe.valid?
@@ -33,6 +34,29 @@ class RecipeTest < ActionDispatch::IntegrationTest
   test "Description should have a maximum length" do
     @recipe.description = "Helo" * 500
     assert_not @recipe.valid?
+  end
+  
+  test "create new valid recipe" do
+    get new_recipe_path
+    recipe_name =  "This is a new recipe!"
+    recipe_description = "Supeer duuper recipe for testing"
+    assert_template 'recipes/new'
+    assert_difference 'Recipe.count' do
+      post recipes_path, params: { recipe: { name: recipe_name, description: recipe_description } }
+    end
+    follow_redirect!  # This one is required when using "redirect_to" method.
+    assert_match recipe_name, response.body
+    assert_match recipe_description, response.body
+  end
+  
+  test "reject invalid recipe submission" do
+    get new_recipe_path
+    assert_template 'recipes/new'
+    assert_no_difference 'Recipe.count' do
+      post recipes_path, params: { recipe: { name: "", description: "" } }
+    end
+    assert_template 'recipes/new'
+    assert_select 'div.alert.alert-danger'
   end
   
 end
